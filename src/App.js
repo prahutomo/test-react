a55506f5a02775f40164833004071291f3374fec
Prabowo Rahutomo <prabowo.rahutomo@customerservice.nsw.gov.au>
2024-11-20 01:11:50 +1100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
 import React, { useState } from 'react';
 
-import Toast from 'react-bootstrap/Toast';
 import Container from 'react-bootstrap/Container';
+import { Routes, Route, Link } from "react-router-dom";
+import {Header} from "./Header"
 
 import './App.css';
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
-
-  return (
-    <Toast show={show} onClose={() => toggleShow(!show)}>
-      <Toast.Header>
-        <strong className="mr-auto">React-Bootstrap</strong>
-      </Toast.Header>
-      <Toast.Body>{children}</Toast.Body>
-    </Toast>
-  );
-};
 
 const App = () => (
   <Container className="p-3">
-    <Container className="p-5 mb-4 bg-light rounded-3">
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast>
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Container>
+    <Header/>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
   </Container>
+  
 );
+
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
 
 export default App;
